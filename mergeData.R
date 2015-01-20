beb9d828e76e71d9e4838199e3ca9fff64a37964
Nazaret <nazasan@hotmail.com>
2015-01-20 21:18:44 +0100

    change in mergeData function
    Including comments

--- mergeData.R
+++ mergeData.R
@@ -1,29 +1,30 @@
 mergeData <- function() {
-        library(data.table)
         ## Read data from directory
         ## Read Test data
         subjectTest <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+        ## Change column name for subjects
         names(subjectTest) <- c("subject")
-        ##subjectTest <-as.data.table(subjectTest)
         XTest <- read.table("./UCI HAR Dataset/test/X_test.txt")
-        ##XTest <-as.data.table(subjectTest)
         YTest <- read.table("./UCI HAR Dataset/test/y_test.txt")
+        ## Change column name for activities
         names(YTest) <- c("activity")
-        ##YTest <-as.data.table(subjectTest)
+        ## Merge all Test data
         dataTest <- cbind(subjectTest,XTest,YTest)
-        ##dataTest <-as.data.table(dataTest)
+        
         ## Read Train data
         subjectTrain <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+        ## Change column name for subjects
         names(subjectTrain) <- c("subject")
-        ##subjectTrain <-as.data.table(subjectTest)
         XTrain <- read.table("./UCI HAR Dataset/train/X_train.txt")
-        ##XTrain <-as.data.table(subjectTest)
         YTrain <- read.table("./UCI HAR Dataset/train/y_train.txt")
+        ## Change column name for activities
         names(YTrain) <- c("activity")
-        ##YTrain <-as.data.table(subjectTest)
+        ## Merge all Train data
         dataTrain <- cbind(subjectTrain,XTrain,YTrain)
-        ##dataTrain <-as.data.table(dataTrain)
+        
         ## Merge in one data set
         dataTotal <- rbind(dataTest,dataTrain)
+        
+        ## Return the complete data set
         dataTotal
 }
