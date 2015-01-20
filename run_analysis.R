071d30537880cb913a48ff0b172dacc065bcd4c8
Nazaret <nazasan@hotmail.com>
2015-01-20 21:27:21 +0100

    change in run_analysis function
    New comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,19 @@
 run_analysis <- function() {
+        ## Include plyr library to use ddply function
         library(plyr)
-        ## Call to the merge function
+        
+        ## Step 1 -> Call to the merge function
         Oridata <- mergeData()
-        ## Call to the extract function
+        ## Step 2 -> Call to the extract function
         FilterData <- extractData(Oridata)
         
+        ## Step 3 -> New data set
         ## Creates a second, independent tidy data set with the average of each variable 
         ## for each activity and each subject.
-        # Run the functions mean on the value of "change" for each group, 
+        # Run the functions mean for each group, 
         # broken down by subject and activity
         AvgData <- ddply(FilterData, .(subject, activity), colwise(mean))
         
-        ## Export the final data to a txt file
+        ## Step 4 -> Export the final data to a txt file
         write.table(AvgData,file="AvgData.txt",row.names=FALSE)
 }
